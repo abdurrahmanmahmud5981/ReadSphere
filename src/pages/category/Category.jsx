f71df512c24dcde25c7bfbac146dd522bce2ddb8
Abdur Rahman Mahmud <ar7543078@gmail.com>
2024-12-23 14:38:51 +0600

    add axiosSecure hook and implement category fetching; update MainLayout and .env for API URL

--- src/pages/category/Category.jsx
+++ src/pages/category/Category.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { axiosSecure } from "../../hooks/useAxiosSecure";
 
 const Category = () => {
-  return (
-    <div>Category</div>
-  )
-}
+  const { category } = useParams();
+  const [booksOfCategory, setBooksOfCategory] = useState([]);
+  console.log(category);
+  useEffect(() => {
+    fetchCategory();
+  }, [category]);
+  const fetchCategory = async () => {
+    try {
+      const { data } = await axiosSecure.get(
+        `/books/categories/${category}`
+      );
+      console.log(data);
+      // setBooksOfCategory(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return <div>Category</div>;
+};
 
-export default Category
+export default Category;
