ac6af8dfed1c3f0d14b8bdbde4309f2b622f5f69
Abdur Rahman (work) <ar7543078@gmail.com>
2025-05-11 16:30:13 +0600

    fix: update card styles in Categories and Features components for consistency

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -36,7 +36,7 @@ const Categories = () => {
         {categories.map(({ title, image, description }) => (
           <div
             key={title}
-            className="card rounded-xl shadow hover:shadow-xl transition-shadow"
+            className="card border rounded-xl shadow hover:shadow-xl transition-shadow"
           >
             <figure>
               <img
--- client/src/components/Features.jsx
+++ client/src/components/Features.jsx
@@ -22,23 +22,23 @@ const Features = () => {
   return (
     <section className="py-16">
       <div className="">
-        <h2 className="text-3xl font-extrabold text-center text-gray-800 mb-12">
+        <h2 className="text-3xl font-extrabold text-center mb-12">
           Why Choose Us
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           {features?.map((item, index) => (
             <div
               key={index}
-              className="card   bg-gradient-to-br from-purple-200/60 via-white/20 to-purple-400/40 shadow-lg hover:shadow-2xl transition duration-300 rounded-xl"
+              className="card   border shadow-lg hover:shadow-2xl transition duration-300 rounded-xl"
             >
               <div className="card-body items-center text-center space-y-4">
                 <div className="rounded-full bg-primary p-5 shadow-lg">
                   {item.icon}
                 </div>
-                <h3 className="card-title text-xl font-bold text-gray-700">
+                <h3 className="card-title text-xl font-bold ">
                   {item.title}
                 </h3>
-                <p className="text-gray-600">{item.description}</p>
+                <p className="">{item.description}</p>
               </div>
             </div>
           ))}
